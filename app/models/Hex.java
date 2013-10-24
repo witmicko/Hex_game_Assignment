f15591cfbb6b94867c848380486f90a3a2e5eb46
witmicko <mjogrodniczak@gmail.com>
2013-10-24 18:32:18 +0100

    cosmetics

--- app/models/Hex.java
+++ app/models/Hex.java
@@ -7,14 +7,13 @@ import javax.persistence.Lob;
 import javax.persistence.Table;
 
 /**
- * Created with IntelliJ IDEA.
+ * This is my Hex class, there are many like it but this one is mine.
  * User: michal
  * Date: 9/28/13
  * Time: 5:14 PM
- * To change this template use File | Settings | File Templates.
  */
 @Entity
-@Table(name = "mahgame")
+@Table(name = "mygame")
 public class Hex extends Model {
 
     @Lob
@@ -28,142 +27,133 @@ public class Hex extends Model {
     // of unions and calculate winner
 
     private int currentPlayer; // Current player in the game, initialised to 1
+    private final int WEST = 0;
+    private final int EAST = 1;
+    private final int NORTH = 2;
+    private final int SOUTH = 3;
+    private final int EMPTY = 0;
+    private final int player1 = 1;
+    private final int player2 = 2;
 
     /**
-     * Constructor for Class Hex, creates 2D array representing board, initialises
-     *
-     * @param n1
-     * @param n2
+     * Parametrized constructor for the Hex class, initialises: empty gameBoard and wquf.
+     * @param n1 width of the game board
+     * @param n2 height of the game board
      */
-
     public Hex(int n1, int n2) // create N-by-N grid, with all sites blocked
     {
         this.n1 = n1;
         this.n2 = n2;
         currentPlayer = 1;
 
-        // TODO: Create instance of board
+        // Create board and set all sites to EMPTY.
         board = new int[n1][n2];
         for (int i = 0; i < n1; i++) {
             for (int j = 0; j < n2; j++) {
-                board[i][j] = 0;
+                board[i][j] = EMPTY;
             }
         }
-
-        // TODO: Create instance WeightedQuickUnionUF class
+        //Create instance WeightedQuickUnionUF class
         wquf = new WeightedQuickUnionUF(n1 * n2 + 4); //4 special sites
-        //wqu[0,1]=special sites for Player1 (imaginary East and West)
-        //wqu[2,3]=special sites for Player2 (imaginary North and South)
-
     }
 
     /**
-     * @param x - x coord on board
-     * @param y - y coord on board
+     * Handles current player`s move, checks if picked site is valid and empty, connects the site with appropriate sites.
+     * @param x - x coord on board(hor)
+     * @param y - y coord on board(ver)
      */
-    // @Override
     public void takeTurn(int x, int y) {
-        //check coords are valid ranges: 0 >= x < n1 AND 0 >= y < n2
+        //check coords are valid
         boolean validCoords = (x < n1 && x >= 0 && y < n2 && y >= 0);
 
-        //will not throw OutOfBounds because it will stop checking condition is coords are invalid ( AND operator )
-        boolean emptySite = (validCoords && board[x][y] == 0);
-
         //check if location is free and set to player's value(1 or 2).
-        if (emptySite) board[x][y] = currentPlayer;
+        boolean isEmptySite = (validCoords && board[x][y] == EMPTY);
 
-        if (validCoords && emptySite) {
-            //calculate location and neighbours location in WeightedQuickUnionUF data structure
-            int indexInWQUF = boardToWquf(x, y);
-
-            //create unions to special sites in WeightedQuickUnionUF that also contain current players value
+        if (isEmptySite) {
+            board[x][y] = currentPlayer;
+            int thisInWquf = boardToWquf(x, y);
             specialSitesCheck(x, y);
-
-            //create unions to neighbour sites in WeightedQuickUnionUF that also contain current players value
-            neighbourSitesCheck(x, y, indexInWQUF);
-            if (!isWinner()) nextPlayer();
-        }
-
-        // in invalid coords or not empty site repeat players round
+            neighbourSitesCheck(x, y, thisInWquf);
 
             // if no winner get the next player
-
+            if (!isWinner()) nextPlayer();
+        }
     }//END takeTurn
 
     /**
-     * @param x
-     * @param y
+     * Checks if selected site is at the board`s edge, connects it to special sites if it does.
+     * @param x - x coord on board(hor)
+     * @param y - y coord on board(ver)
      */
     private void specialSitesCheck(final int x, final int y) {
-        if (currentPlayer == 1 && x == 0) {
-            wquf.union(0, boardToWquf(x, y));
-        }
-        if (currentPlayer == 1 && x == n1 - 1) {
-            wquf.union(1, boardToWquf(x, y));
-        }
+        int thisSite=boardToWquf(x, y);
 
-        if (currentPlayer == 2 && y == 0) {
-            wquf.union(2, boardToWquf(x, y));
-        }
-        if (currentPlayer == 2 && y == n2 - 1) {
-            wquf.union(3, boardToWquf(x, y));
+        if (currentPlayer == player1) {
+            if (x == 0) wquf.union(WEST, thisSite);
+            else if (x == n1 - 1) wquf.union(EAST, thisSite);
+        } else{
+            if (y == 0) wquf.union(NORTH, thisSite);
+            else if (y == n2 - 1) wquf.union(SOUTH, thisSite);
         }
     }
 
     /**
-     * @param x
-     * @param y
-     * @param indexInWQUF
+     * Checks neighbor sites and connects to this site is they belong to current player.
+     * @param x - x coord on board(hor)
+     * @param y - y coord on board(ver)
+     * @param thisInWquf
      */
-    private void neighbourSitesCheck(final int x, final int y, final int indexInWQUF) {
-        if (y > 0 && board[x][y - 1] == currentPlayer) {
-            wquf.union(boardToWquf(x, y - 1), indexInWQUF);
+    private void neighbourSitesCheck(final int x, final int y, final int thisInWquf) {
+
+        if ((y > 0) && board[x][y - 1] == currentPlayer) {
+            wquf.union(boardToWquf(x, y - 1), thisInWquf);
         }
-        if (x < n1 - 1 && y < n2 - 1 && board[x + 1][y + 1] == currentPlayer) {
-            wquf.union(boardToWquf(x + 1, y + 1), indexInWQUF);
+
+        if ((y < n2 - 1) && board[x][y + 1] == currentPlayer) {
+            wquf.union(boardToWquf(x, y + 1), thisInWquf);
         }
-        if (x < n1 - 1 && board[x + 1][y] == currentPlayer) {
-            wquf.union(boardToWquf(x + 1, y), indexInWQUF);
+        // if x>=n1(width) there is no sites to the right of it.
+        if ((x < n1 - 1)) {
+            if ((y < n2 - 1) && (board[x + 1][y + 1]) == currentPlayer) {
+                wquf.union(boardToWquf(x + 1, y + 1), thisInWquf);
+            }
+            if (board[x + 1][y] == currentPlayer) {
+                wquf.union(boardToWquf(x + 1, y), thisInWquf);
+            }
         }
-        if (y < n2 - 1 && board[x][y + 1] == currentPlayer) {
-            wquf.union(boardToWquf(x, y + 1), indexInWQUF);
-        }
-        if (x > 0 && y < n2 - 1 && board[x - 1][y + 1] == currentPlayer) {
-            wquf.union(boardToWquf(x - 1, y + 1), indexInWQUF);
-        }
-        if (x > 0 && board[x - 1][y] == currentPlayer) {
-            int one = boardToWquf(x - 1, y);
-            wquf.union(one, indexInWQUF);
+        //if x <= 0 there is no sites to the left of it.
+        if (x > 0) {
+            if ((y < n2 - 1) && (board[x - 1][y + 1]) == currentPlayer) {
+                wquf.union(boardToWquf(x - 1, y + 1), thisInWquf);
+            }
+            if (board[x - 1][y] == currentPlayer) {
+                wquf.union(boardToWquf(x - 1, y), thisInWquf);
+            }
         }
     }
 
     /**
-     * @param nextPlayer
+     * Changes current player to next player.
+     * @param
      */
     private void nextPlayer() {
-        setCurrentPlayer((currentPlayer == 2) ? 1 : 2);
-
+        setCurrentPlayer((currentPlayer == player2) ? player1 : player2);
     }
 
     /*
-     * (non-Javadoc)
-     *
-     * @see BoardGame#isWinner()
-     */
-    //@Override
+    * (non-Javadoc)
+    *
+    * @see BoardGame#isWinner()
+    */
     public boolean isWinner() {
-        // TODO:check if there is a connection between either side of the board.
+        // check if there is a connection between either side of the board.
         // You can do this by using the 'virtual site' approach in the
         // percolation test.
-        boolean isWinner = false;
-        if (currentPlayer == 1) isWinner = wquf.connected(0, 1);
-        if (currentPlayer == 2) isWinner = wquf.connected(2, 3);
-
-        //printBoard();
-        return isWinner;
+        return wquf.connected(EAST, WEST) || wquf.connected(NORTH, SOUTH);
     }
 
     /**
+     * Standard setter for current player.
      * @param currentPlayer
      */
     private void setCurrentPlayer(int currentPlayer) {
@@ -171,83 +161,37 @@ public class Hex extends Model {
     }
 
     /**
-     * @return
+     * Getter for current player
+     * @return current player`s number
      */
-    // @Override
     public int getCurrentPlayer() {
         return currentPlayer;
     }
 
-
     /**
-     * @return
+     * Getter for board
+     * @return gameboard
      */
-    //@Override
     public int[][] getBoard() {
         return board;
     }
 
     /**
-     * THIS IS OPTIONAL:
-     * Modify the main method if you wish to suit your implementation.
-     * This is just an example of a test implementation.
-     * For example you may want to display the board after each turn.
-     *
-     * @param args
-     */
-//    public static void main(String[] args) {
-//
-//        BoardGame hexGame = new Hex(5, 5);
-//
-//        while (!hexGame.isWinner()) {
-//            printBoard(hexGame.getBoard());
-//            System.out.println("It's player " + hexGame.getCurrentPlayer() + "'s turn");
-//            System.out.println("Enter x and y location:");
-//            int x = StdIn.readInt();
-//            int y = StdIn.readInt();
-//
-//            hexGame.takeTurn(x, y);
-//
-//        }
-//
-//        System.out.println("It's over. Player " + hexGame.getCurrentPlayer()
-//                + " wins!");
-//
-//    }
-
-    /**
-     * @param x
-     * @param y
-     * @return
+     * Converts index of 2D array (board) into index of flat array in WQU
+     * @param x board[x][y] x index of board
+     * @param y board[x][y] y index of board
+     * @return equivalent index in flat array
      */
     private int boardToWquf(int x, int y) {
         return x * n1 + y + 4;//+4 for 4 special sites (North-South-West-East)
     }
 
     /**
-     *
+     * gets colour of given site
+     * @param x board[x][y] x index of board
+     * @param y board[x][y] y index of board
+     * @return color
      */
-    private static void printBoard(int[][] board) {
-
-        String str = "_____";
-        for (int i = 0; i < board.length; i++) {
-            String s = (i < 10) ? "0" : "";
-            str += "|_" + s + i + "_";
-        }
-        str += "|";
-
-        System.out.println(str);
-        for (int i = 0; i < board[0].length; i++) {
-            String s = (i < 10) ? "0" : "";
-            System.out.print("|_" + s + i + "_");
-
-            for (int j = 0; j < board.length; j++) {
-                System.out.print("|__" + board[j][i] + "_");
-            }
-            System.out.println("|");
-        }
-    }
-
     public String getColor(int x, int y) {
         String color = "#dfdfdf";
         if (board[x][y] == 1) color = "blue";
@@ -255,7 +199,6 @@ public class Hex extends Model {
 
         return color;
     }
-
 }
 
 
